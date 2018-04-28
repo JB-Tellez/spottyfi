5a4b38800a2e35a5957c2d2a6b6de122a2b1a889
JB Tellez <jb.tellez@gmail.com>
2018-04-28 09:49:04 -0700

    more skeletony

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
-
 app.get('/api/ping', (req, res) => res.send('pong'));
 
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
